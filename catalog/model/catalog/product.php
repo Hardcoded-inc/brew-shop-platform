7e04d8de433842b970738ea47039d90931e1ff46
Oskar Legner <oskarlegner@gmail.com>
2023-05-01 17:06:07 +0200

    Get rid of categories in search feature

--- catalog/model/catalog/product.php
+++ catalog/model/catalog/product.php
@@ -20,45 +20,11 @@ class Product extends \Opencart\System\Engine\Model {
 	}
 
 	public function getProducts(array $data = []): array {
+
 		$sql = "SELECT p.`product_id`, (SELECT AVG(`rating`) AS `total` FROM `" . DB_PREFIX . "review` r1 WHERE r1.`product_id` = p.`product_id` AND r1.`status` = '1' GROUP BY r1.`product_id`) AS `rating`, (SELECT `price` FROM `" . DB_PREFIX . "product_discount` pd2 WHERE pd2.`product_id` = p.`product_id` AND pd2.`customer_group_id` = '" . (int)$this->config->get('config_customer_group_id') . "' AND pd2.`quantity` = '1' AND ((pd2.`date_start` = '0000-00-00' OR pd2.`date_start` < NOW()) AND (pd2.`date_end` = '0000-00-00' OR pd2.`date_end` > NOW())) ORDER BY pd2.`priority` ASC, pd2.`price` ASC LIMIT 1) AS `discount`, (SELECT `price` FROM `" . DB_PREFIX . "product_special` ps WHERE ps.`product_id` = p.`product_id` AND ps.`customer_group_id` = '" . (int)$this->config->get('config_customer_group_id') . "' AND ((ps.`date_start` = '0000-00-00' OR ps.`date_start` < NOW()) AND (ps.`date_end` = '0000-00-00' OR ps.`date_end` > NOW())) ORDER BY ps.`priority` ASC, ps.`price` ASC LIMIT 1) AS `special`";
-
-		if (!empty($data['filter_category_id'])) {
-			if (!empty($data['filter_sub_category'])) {
-				$sql .= " FROM `" . DB_PREFIX . "category_path` cp LEFT JOIN `" . DB_PREFIX . "product_to_category` p2c ON (cp.`category_id` = p2c.`category_id`)";
-			} else {
-				$sql .= " FROM `" . DB_PREFIX . "product_to_category` p2c";
-			}
-
-			if (!empty($data['filter_filter'])) {
-				$sql .= " LEFT JOIN `" . DB_PREFIX . "product_filter` pf ON (p2c.`product_id` = pf.`product_id`) LEFT JOIN `" . DB_PREFIX . "product` p ON (pf.`product_id` = p.`product_id`)";
-			} else {
-				$sql .= " LEFT JOIN `" . DB_PREFIX . "product` p ON (p2c.`product_id` = p.`product_id`)";
-			}
-		} else {
-			$sql .= " FROM `" . DB_PREFIX . "product` p";
-		}
-
+		$sql .= " FROM `" . DB_PREFIX . "product` p";
 		$sql .= " LEFT JOIN `" . DB_PREFIX . "product_description` pd ON (p.`product_id` = pd.`product_id`) LEFT JOIN `" . DB_PREFIX . "product_to_store` p2s ON (p.`product_id` = p2s.`product_id`) WHERE pd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND p.`status` = '1' AND p.`date_available` <= NOW() AND p2s.`store_id` = '" . (int)$this->config->get('config_store_id') . "'";
 
-		if (!empty($data['filter_category_id'])) {
-			if (!empty($data['filter_sub_category'])) {
-				$sql .= " AND cp.`path_id` = '" . (int)$data['filter_category_id'] . "'";
-			} else {
-				$sql .= " AND p2c.`category_id` = '" . (int)$data['filter_category_id'] . "'";
-			}
-
-			if (!empty($data['filter_filter'])) {
-				$implode = [];
-
-				$filters = explode(',', $data['filter_filter']);
-
-				foreach ($filters as $filter_id) {
-					$implode[] = (int)$filter_id;
-				}
-
-				$sql .= " AND pf.`filter_id` IN (" . implode(',', $implode) . ")";
-			}
-		}
 
 		if (!empty($data['filter_name']) || !empty($data['filter_tag'])) {
 			$sql .= " AND (";
@@ -69,7 +35,7 @@ class Product extends \Opencart\System\Engine\Model {
 				$words = explode(' ', trim(preg_replace('/\s+/', ' ', $data['filter_name'])));
 
 				foreach ($words as $word) {
-					$implode[] = "pd.`name` LIKE '" . $this->db->escape('%' . $word . '%') . "'";
+					$implode[] = "pd.`name` LIKE '" . '%' . $word . '%' . "'";
 				}
 
 				if ($implode) {
@@ -77,7 +43,7 @@ class Product extends \Opencart\System\Engine\Model {
 				}
 
 				if (!empty($data['filter_description'])) {
-					$sql .= " OR pd.`description` LIKE '" . $this->db->escape('%' . (string)$data['filter_name'] . '%') . "'";
+					$sql .= " OR pd.`description` LIKE '" . '%' . (string)$data['filter_name'] . '%' . "'";
 				}
 			}
 
@@ -91,7 +57,7 @@ class Product extends \Opencart\System\Engine\Model {
 				$words = explode(' ', trim(preg_replace('/\s+/', ' ', $data['filter_tag'])));
 
 				foreach ($words as $word) {
-					$implode[] = "pd.`tag` LIKE '" . $this->db->escape('%' . $word . '%') . "'";
+					$implode[] = "pd.`tag` LIKE '" . '%' . $word . '%' . "'";
 				}
 
 				if ($implode) {
@@ -100,16 +66,19 @@ class Product extends \Opencart\System\Engine\Model {
 			}
 
 			if (!empty($data['filter_name'])) {
-				$sql .= " OR LCASE(p.`model`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
-				$sql .= " OR LCASE(p.`sku`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
-				$sql .= " OR LCASE(p.`upc`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
-				$sql .= " OR LCASE(p.`ean`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
-				$sql .= " OR LCASE(p.`jan`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
-				$sql .= " OR LCASE(p.`isbn`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
-				$sql .= " OR LCASE(p.`mpn`) = '" . $this->db->escape(oc_strtolower($data['filter_name'])) . "'";
+				$sql .= " OR LCASE(p.`model`) = '" . oc_strtolower($data['filter_name']) . "'";
+				$sql .= " OR LCASE(p.`sku`) = '" . oc_strtolower($data['filter_name']) . "'";
+				$sql .= " OR LCASE(p.`upc`) = '" . oc_strtolower($data['filter_name']) . "'";
+				$sql .= " OR LCASE(p.`ean`) = '" . oc_strtolower($data['filter_name']) . "'";
+				$sql .= " OR LCASE(p.`jan`) = '" . oc_strtolower($data['filter_name']) . "'";
+				$sql .= " OR LCASE(p.`isbn`) = '" . oc_strtolower($data['filter_name']) . "'";
+				$sql .= " OR LCASE(p.`mpn`) = '" . oc_strtolower($data['filter_name']) . "'";
 			}
 
 			$sql .= ")";
+
+			echo($sql);
+
 		}
 
 		if (!empty($data['filter_manufacturer_id'])) {
@@ -163,11 +132,14 @@ class Product extends \Opencart\System\Engine\Model {
 		$query = $this->db->query($sql);
 
 		foreach ($query->rows as $result) {
+			echo("--------------------------------------------------------------------------------------------------------------");
+			print_r($result);
 			// for never get one more time with same product id
 			if (!isset($product_data[$result['product_id']])) {
 				$product_data[$result['product_id']] = $this->model_catalog_product->getProduct($result['product_id']);
 			}
 		}
+
 
 		return $product_data;
 	}
@@ -368,6 +340,7 @@ class Product extends \Opencart\System\Engine\Model {
 	}
 
 	public function getTotalProducts(array $data = []): int {
+
 		$sql = "SELECT COUNT(DISTINCT p.`product_id`) AS total";
 
 		if (!empty($data['filter_category_id'])) {
